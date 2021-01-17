30b9e9677b068ae4a734d3bc812cac8e158d74f0
xljiulang <366193849@qq.com>
2021-01-17 14:00:17 +0800

    增加MemoryBufferWriter类型

---
+++ MemoryExtensions/MemoryBufferWriterExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace System.Buffers
+{
+    /// <summary>
+    /// 提供MemoryBufferWriter的创建扩展
+    /// </summary>
+    public static class MemoryBufferWriterExtensions
+    {
+        /// <summary>
+        /// 创建固定大小的BufferWriter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static MemoryBufferWriter<T> CreateWriter<T>(this T[] array)
+        {
+            return array.AsMemory().CreateWriter();
+        }
+
+        /// <summary>
+        /// 创建固定大小的BufferWriter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arraySegment"></param>
+        /// <returns></returns>
+        public static MemoryBufferWriter<T> CreateWriter<T>(this ArraySegment<T> arraySegment)
+        {
+            return arraySegment.AsMemory().CreateWriter();
+        }
+
+        /// <summary>
+        /// 获取BufferWriter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="memory"></param>
+        /// <returns></returns>
+        public static MemoryBufferWriter<T> CreateWriter<T>(this Memory<T> memory)
+        {
+            return new MemoryBufferWriter<T>(memory);
+        }
+    }
+}
